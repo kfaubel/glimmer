36cad7ba4fd8933703e03d76c1abfe2b253e35be
Ken Faubel <ken@faubel.org>
2022-01-01 10:55:53 -0500

    Minor updates

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
 import ReactDOM from 'react-dom';
-import './index.css';
+//import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Sequence } from './Sequence';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const sequence = new Sequence("ken");
 sequence.start(); // Loads the screen list, sets an interval timer to update each screen image.
